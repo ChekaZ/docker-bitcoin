8b90c4f31750ba9794e5ce726ecbfef2a652570b
Oliver Ziegler <o.zieglerZ@web.de>
2018-06-27 21:31:11 +0200

    Feathercoin update

--- feathercoin/0.16.0/docker-entrypoint.sh
+++ feathercoin/0.16.0/docker-entrypoint.sh
@@ -2,23 +2,23 @@
 set -e
 
 if [[ "$1" == "feathercoin-cli" || "$1" == "feathercoin-tx" || "$1" == "feathercoind" || "$1" == "test_feathercoin" ]]; then
-	mkdir -p "$BITCOIN_DATA"
+	mkdir -p "$FEATHERCOIN_DATA"
 
-	cat <<-EOF > "$BITCOIN_DATA/feathercoin.conf"
+	cat <<-EOF > "$FEATHERCOIN_DATA/feathercoin.conf"
 	printtoconsole=1
 	rpcallowip=::/0
-	${BITCOIN_EXTRA_ARGS}
+	${FEATHERCOIN_EXTRA_ARGS}
 	EOF
-	chown bitcoin:bitcoin "$BITCOIN_DATA/feathercoin.conf"
+	chown feathercoin:feathercoin "$FEATHERCOIN_DATA/feathercoin.conf"
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
-	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.feathercoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.feathercoin
+	chown -R feathercoin "$FEATHERCOIN_DATA"
+	ln -sfn "$FEATHERCOIN_DATA" /home/feathercoin/.feathercoin
+	chown -h feathercoin:feathercoin /home/feathercoin/.feathercoin
 
-	exec gosu bitcoin "$@"
+	exec gosu feathercoin "$@"
 else
 	exec "$@"
 fi
